0da6dfc9a495a89f5b24efe922970b05957158de
vincent munywoki <vincentmunywoki12@gmail.com>
2024-11-16 12:00:35 +0300

    intasend payment

--- ONLINE-PAYMENT/script.js
+++ ONLINE-PAYMENT/script.js
@@ -5,3 +5,12 @@ new window.IntaSend({ // new instance as new popup
     .on("COMPLETE", (results) => {console.log("Do something on success", results)}) // successful payment
     .on("FAILED", (results) => {console.log("Do something on failure", results)})  // failed payment
     .on("IN-PROGRESS", (results) => {console.log("Payment in progress status", results)}) // not complete and not failed.
+
+
+    new window.IntaSend({ // new instance as new popup
+        publicAPIKey: "ISPubKey_live_91dcad9a-ec13-4140-a28b-c3a6dc5fb2fc",
+        live: true //set to true when going live
+        })
+        .on("COMPLETE", (results) => {console.log("Do something on success", results)}) // successful payment
+        .on("FAILED", (results) => {console.log("Do something on failure", results)})  // failed payment
+        .on("IN-PROGRESS", (results) => {console.log("Payment in progress status", results)}) // not complete and not failed.
